771e91aef1cc4cfe0af5e43b36bbee7699bbdab1
Fernando Enrique Arias Carrasco <quique11121enrique@outlook.com>
2021-05-04 19:39:17 -0400

    examen 2

--- Evaluacion/Evaluacion/Data/Entities/User.cs
+++ Evaluacion/Evaluacion/Data/Entities/User.cs
@@ -7,15 +7,17 @@ namespace Evaluacion.Data.Entities
     public class User
     {
         public int Id { get; set; }
+        [MaxLength(21)]
 
         public string Name { get; set; }
+        [Required,EmailAddress]
         public string Address { get; set; }
+        [Required, Phone]
         public string Phone { get; set; }
-
         public string Nickname { get; set; }
-
+        [Required,MinLength(9)]
         public string Password { get; set; }
-
+        [Required]
         public bool StatusLog { get; set; }
     }
 }
